16e71afea6b2ac7d6555cc3edbaff0d35556ced4
gaoch <gaospecial@gmail.com>
2019-05-17 17:02:34 +0800

    热心肠日报分析中使用了list of list数据结构，目前对其操作仍然比较棘手。正在使用小数据测试。

---
+++ rvest/list_of_list.R
@@ -0,0 +1,10 @@
+a <- list(id=1,title="great idea",abstract="First,\n;Second:\nand then do it.")
+a
+write.csv(a,file = "a.csv")
+write.csv(a,file = "a.csv",row.names = F)
+aaa <- list(a,a,a)
+b <- do.call("rbind",aaa)
+as_tibble(b)
+as_tibble(data.frame(b))
+a_safe <- lapply(a, function(x)gsub("\\s"," ",x))
+aaa_safe <- lapply(aaa,function(a)lapply(a,function(x)gsub("\\s"," ",x)))
